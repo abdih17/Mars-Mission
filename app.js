45295b51c321d698486cd6d7f956fe169a4cd5c7
Colin Lamont <colinlamont@gmail.com>
2016-10-10 12:21:14 -0700

    partially functioning delayedText function

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 'use strict';
 
-var players = [];
 
-function Player (loginName, password, playerName) {
-  this.login = loginName;
-  this.password = password;
-  this.playName = playerName;
-  this.oxygen = 4;
-  this.water = 4;
-  players.push(this);
+function check(form){
+  if(form.userid.value == 'mars' && form.pswrd.value == 'abc')
+  { window.open('https://github.com/abdih17/Mars-Mission'); //website it will transfer over to
+  } else {
+    alert('Please sign-in using the correct username and password.'); //or another message?
+  }
 };
 
-// function check(form){
-//   if(form.userid.value == 'mars' && form.pswrd.value == 'abc')
-//   { window.open('https://github.com/abdih17/Mars-Mission'); //website it will transfer over to
-//   } else {
-//     alert('Please sign-in using the correct username and password.'); //or another message?
-//   }
-// };
-
-
-new Player('Jonny', '1234', 'Mark');
-
-// check();
+check();
 //add create an account
+
+
+// create function to display delayed text
+function marsText() {
+
+  var delayedText = function(target, msg, index, interval){
+
+    var el = document.getElementById(target);
+
+    if(index < msg.length){
+      el.textContent = el.textContent + msg.charAt(index);
+      index = index + 1;
+      setTimeout(function(){
+        delayedText(target,msg,index,interval);
+      },interval);
+    }
+
+  };
+
+  delayedText('delayedText', 'Welcome to Mars Mission. You must survive.', 0, 70);
+}
+marsText();
+
+// html needs to use id="delayedText"

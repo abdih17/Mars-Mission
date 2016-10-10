c40a8885a8a7a21689a49605211672338caea002
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-10-10 11:47:36 -0700

    create player object

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 'use strict';
 
+var players = [];
 
-function check(form){
-  if(form.userid.value == 'mars' && form.pswrd.value == 'abc')
-  { window.open('https://github.com/abdih17/Mars-Mission'); //website it will transfer over to
-  } else {
-    alert('Please sign-in using the correct username and password.'); //or another message?
-  }
+function Player (loginName, password, playerName) {
+  this.login = loginName;
+  this.password = password;
+  this.playName = playerName;
+  this.oxygen = 4;
+  this.water = 4;
+  players.push(this);
 };
 
-check();
+// function check(form){
+//   if(form.userid.value == 'mars' && form.pswrd.value == 'abc')
+//   { window.open('https://github.com/abdih17/Mars-Mission'); //website it will transfer over to
+//   } else {
+//     alert('Please sign-in using the correct username and password.'); //or another message?
+//   }
+// };
+
+
+new Player('Jonny', '1234', 'Mark');
+
+// check();
 //add create an account

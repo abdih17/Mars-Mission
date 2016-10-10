ecfc7262058d9c9ee4dbbd69c291a64d2b4dcd25
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-10-10 16:30:09 -0700

    print questions to page

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 'use strict';
 var players = [];
+var text = document.getElementById('text_wrapper');
+var question = document.getElementById('question_wrapper');
+var answer = document.getElementById('answers_wrapper');
+var currentQuestion = 0;
+var questions = [];
+
 function Player (loginName, password, playerName) {
   this.login = loginName;
   this.password = password;
@@ -10,6 +16,17 @@ function Player (loginName, password, playerName) {
   players.push(this);
 };
 
+var wakeUp = {
+  text: 'Beep...Beep...Beep... Oxygen critically low... Oxygen critically low...',
+  question: 'What is your name?',
+};
+var getToBase = {
+  text: 'After choosing to abort the mission and flee Mars due to the storm your team left the base. The base by design is self locking. In order to get inside you must enter the door code that was given to you on the previous question.',
+  question: 'What is the code?',
+};
+
+questions.push(wakeUp, getToBase);
+
 // check and load local storage
 if (localStorage.getItem('playersData')){
   players = [];
@@ -18,7 +35,7 @@ if (localStorage.getItem('playersData')){
 //event listener for player login
   var login = document.getElementById('submit_login');
 
-  function getUserLogin(event){
+  function getUserLogin(){
     var player = createUser.username.value;
     var password = createUser.password.value;
     new Player (player,password);
@@ -33,6 +50,33 @@ var setLocalStorage = function() {
   var playersString = JSON.stringify(players);
   localStorage.setItem('playersData', playersString);
 };
+
+
+
+var formEl = document.createElement('form');
+var inputEl = document.createElement('input');
+var buttonEl = document.createElement('button');
+var pEl = document.createElement('p');
+var divEl = document.createElement('div');
+
+function printText () {
+  pEl.textContent = questions[currentQuestion].text;
+  divEl.appendChild(pEl);
+  text.appendChild(divEl);
+}
+printText();
+// function questionText () {
+//   pEl.textContent = questions[currentQuestion].question;
+//   divEl.appendChild(pEl);
+//   question.appendChild(divEl);
+// }
+// questionText();
+// function answerText () {
+//   pEl.textContent = 'Answers';
+//   divEl.appendChild(pEl);
+//   answer.appendChild(divEl);
+// }
+// answerText();
 
 // function check(form){
 //   if(form.userid.value == 'mars' && form.pswrd.value == 'abc')
